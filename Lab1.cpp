c898ab2c5c358396fbf48e284c96986e45228dfc
kill-bcv <127349025+kill-bcv@users.noreply.github.com>
2023-09-19 22:00:48 +0300

    Add files via upload

---
+++ Lab1.cpp
@@ -0,0 +1,42 @@
+﻿#include <vector>
+
+// Перечисление направлений движений
+enum Direction {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+};
+
+// Координаты на игровом поле
+struct Coordinate {
+    int x;
+    int y;
+};
+
+// Для создания ячеек
+struct GameCell {
+    Coordinate position;
+    bool isOccupied; // будет проверяться занята ячейка или нет
+    
+};
+
+// Структура для представления змейки
+struct Snake {
+    std::vector<Coordinate> body; // Данамичесий вектор координат для тела змейки
+    Direction direction; // Направление движения змейки
+    int length;
+};
+
+// Игровое поле
+struct GameBoard {
+    int width;
+    int height;
+    std::vector<GameCell> cells; // Вектор ячеек игрового поля
+    Snake snake;
+};
+
+int main() {
+
+    return 0;
+}
